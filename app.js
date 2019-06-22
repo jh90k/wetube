02d12de3c5920e71ca4f20543d7a5bc432a18dac
Jenna <47073356+jh90k@users.noreply.github.com>
2019-06-22 15:14:18 +0900

    2_12

--- app.js
+++ app.js
@@ -18,21 +18,30 @@ const app = express();
 /*above thing, we call it arrow function*/
 
 app.use(cookieParser());
+//쿠키는 사용자 인증 시 사용되는데, 쿠키 파서 덕에 쿠키를 사용할 수 있음
 app.use(bodyParser.json({extended: true}));
 /*서버가 json을 이해하게 만드는 코드*/
 app.use(bodyParser.urlencoded({extended: true}));
 /*서버가 form에서 받은 데이터를 이해하게 만드는 코드*/
+//아바타의 사진 등 사용자 정보 전송
 app.use(morgan("dev"));
+//loggin every event that is caused at app.
 app.use(helmet());
+//helmet makes app more safe
 
 const middleware = (req, res, next) => {
     res.send("not happening")
 };
 
-app.use("/", globalRouter);
+/*app.use("/", globalRouter);
 app.use("routes.user", userRouter);
 //app.use means somebody goes to /user, i'm gonna use whole router.js
-app.use("routes.videos", videoRouter);
+app.use("routes.videos", videoRouter);*/
+globalRouter.get(routes.home, home);
+globalRouter.get(routes.search, search);
+globalRouter.get(routes.join, join);
+globalRouter.get(routes.login, login);
+globalRouter.get(routes.logout, logout);
 
 
 export default app;
