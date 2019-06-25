fdf3503c4f934f74522c4fbe31c3e11fa66fb30e
Jenna <47073356+jh90k@users.noreply.github.com>
2019-06-25 20:42:40 +0900

    2_21

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,12 +1,12 @@
 import express from "express";
 import routes from "../routes";
 // '../' means outside the folder
-import { upload, videoDetail, editVideo, deleteVideo, videos } from "../controllers/videoController.js";
+import { upload, videoDetail, editVideo, deleteVideo } from "../controllers/videoController.js";
 
 const videoRouter = express.Router();
 
 //아랫줄처럼 수정 videoRouter.get(routes.videos, videos);
-videoRouter.get(routes.videos, videos);
+
 videoRouter.get(routes.upload, upload);
 videoRouter.get(routes.videoDetail, videoDetail);
 videoRouter.get(routes.editVideo, editVideo);
